9375310918ca7cda5235bf21313fa858451ef15f
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2022-10-04 20:18:55 +0200

    v01

---
+++ README.md
@@ -0,0 +1,16 @@
+20221001 Samstagsprojekt lev3_1 MwSt-Rechner
+
+Link zur Seite: https://weimingtay.github.io/20221001_Samstag_Mwst-Rechner/
+
+# Aufgabenstellung
+
+
+In diesem Projekt machen wir eine Mehrwertsteuer-Rechner-App.
+
+In Deutschland gibt es zwei Mehrwertsteuersätze: 19 % und 7 %.
+
+Mit dieser App können wir Netto zu Brutto oder Brutto zu Netto rechnen.
+
+## Ergebnisvorschau
+
+![](assets/img/Bildschirmfoto%202022-10-01%20um%2009.11.12.png)
---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// Variablen bestimmen
+const nettoBrut = document.getElementById("nettoToBrutto");
+const bruttoNet = document.getElementById("bruttoToNetto");
+
+const mwst19 = document.getElementById("mwst19");
+const mwst7 = document.getElementById("mwst7");
+
+let eingabe = document.getElementById("bruttoEingabe");
+let ausgabeMwst = document.getElementById("mwst");
+let ausgabeGesamt = document.getElementById("ausgabe");
+
+let eingabeWert = document.getElementById("eingabeWert");
+let ausgabeWert = document.getElementById("gesamtWert");
+
+
+// Texte werden angepasst
+
+function bruttoNetto() {
+    if (bruttoNet.checked) {
+        eingabeWert.innerHTML="Bruttobetrag in Euro"
+        ausgabeWert.innerHTML="Nettobetrag"
+    } else if (nettoBrut.checked) {
+        eingabeWert.innerHTML="Nettobetrag in Euro"
+        ausgabeWert.innerHTML="Bruttobetrag (Endpreis)"
+    }
+}
+
+// Mwst-Satz wird berechnet
+
+function submit() {
+    if (nettoBrut.checked && mwst19.checked) {
+        ausgabeGesamt= eingabe.value * 1.19,
+        ausgabe.innerHTML=ausgabeGesamt.toFixed(2)+" €",
+
+        ausgabeMwst=eingabe.value*0.19,
+        mwst.innerHTML=ausgabeMwst.toFixed(2)+" €"
+    } else if (nettoBrut.checked && mwst7.checked) {
+        ausgabeGesamt=eingabe.value * 1.07,
+        ausgabe.innerHTML=ausgabeGesamt.toFixed(2)+" €"
+        
+        ausgabeMwst=eingabe.value*0.07,
+        mwst.innerHTML=ausgabeMwst.toFixed(2)+" €"
+    } else if (bruttoNet.checked && mwst19.checked) {
+        ausgabeGesamt=eingabe.value/1.19,
+        ausgabe.innerHTML=ausgabeGesamt.toFixed(2)+" €"
+
+        ausgabeMwst=eingabe.value/119*19,
+        mwst.innerHTML=ausgabeMwst.toFixed(2)+" €"
+    } else if (bruttoNet.checked && mwst7.checked) {
+        ausgabeGesamt=eingabe.value/1.07,
+        ausgabe.innerHTML=ausgabeGesamt.toFixed(2)+" €",
+
+        ausgabeMwst=eingabe.value/107*7,
+        mwst.innerHTML=ausgabeMwst.toFixed(2)+" €"
+    }
+}
